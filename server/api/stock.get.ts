a656322a07b03a9e7b77fd18785825544e25677a
Muhammad Fayaad <fayet256@gmail.com>
2023-08-11 22:36:34 +0700

    fix: update stock api

--- server/api/stock.get.ts
+++ server/api/stock.get.ts
@@ -1,12 +1,10 @@
 export default defineEventHandler(async (event) => {
-  if (!process.dev) return { disabled: true };
-
   setHeader(event, "cache-control", "no-cache");
   setHeader(event, "connection", "keep-alive");
   setHeader(event, "content-type", "text/event-stream");
   setResponseStatus(event, 200);
 
-  let counter = 0;
+  const counter = 0;
   sendEvent(event, counter);
 
   event._handled = true;
